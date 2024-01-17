38d7518a9f4881ca107b38ebb0dbbd68a6750c02
vern wolfley <vern.wolfley@gmail.com>
2024-01-16 19:03:52 -0700

    update for lesson 02

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,17 @@
+//toLocaleDateString
+const options = { weekday: "long", year: "numeric", month: "long", day: "numeric"};
+const options2 = { weekday: "short", year: "numeric", month: "short", day: "numeric"};
+
+const date = new Date()
+
+// let modDate = date.toLocaleDateString("en-US", options2);
+// document.getElementById("lastModified").innerHTML = `Last Modified: ${modDate}`;
+
+let copyrightYear = date.getFullYear();
+document.getElementById("cYear").innerHTML = `&copy; ${copyrightYear}`;
+
+let lastModif = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModif}`;
+
+// console.log(modDate);
+// console.log(copyrightYear);
