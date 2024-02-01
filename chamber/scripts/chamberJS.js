195991cb2ef62dbec2bc755ab071345bab31aed1
vern wolfley <vern.wolfley@gmail.com>
2024-02-01 15:44:53 -0700

    update add icons

---
+++ chamber/scripts/chamberJS.js
@@ -0,0 +1,14 @@
+//toLocaleDateString
+const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+const options2 = { weekday: "short", year: "numeric", month: "short", day: "numeric" };
+
+const date = new Date();
+
+// let modDate = date.toLocaleDateString("en-US", options2);
+// document.getElementById("lastModified").innerHTML = `Last Modified: ${modDate}`;
+
+let copyrightYear = date.getFullYear();
+document.getElementById("cYear").innerHTML = `&copy; ${copyrightYear}`;
+
+let lastModif = new Date(document.lastModified).toGMTString();
+document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModif}`;
