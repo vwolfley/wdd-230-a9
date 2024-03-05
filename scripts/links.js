1983eaf0ac284c6fe87b97d88421a0a83e115893
vern wolfley <vern.wolfley@gmail.com>
2024-03-04 17:40:52 -0700

    add links to page from js

--- scripts/links.js
+++ scripts/links.js
@@ -3,18 +3,46 @@ const linksURL = "https://vwolfley.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     try {
-        const response = await fetch(baseURL);
+        const response = await fetch(linksURL);
         if (!response.ok) {
             throw new Error("Failed to fetch data");
         }
         const data = await response.json();
-        console.log(data);
-        // displayProphets(data.prophets);
+        // console.log(data);
+        displayLinks(data.lessons);
         return data;
     } catch (error) {
         console.error("Error fetching data:", error);
         throw error;
     }
 }
-
 getLinks();
+
+const displayLinks = (weeks) => {
+    // console.log(weeks);
+
+    weeks.forEach((week) => {
+        // console.log(week);
+        const listItem = document.createElement("li");
+        const lessonWeek = document.createTextNode(`Lesson ${week.lesson}: `);
+
+        listItem.appendChild(lessonWeek);
+
+        week.links.forEach((link) => {
+            // console.log(link);
+            const tag = document.createElement("a");
+            tag.setAttribute("href", link.url);
+            tag.textContent = link.title;
+
+            listItem.appendChild(tag);
+
+            if (week.links.length > 1 && week.links.indexOf(link) < week.links.length - 1) {
+                listItem.innerHTML += " | ";
+            }
+        });
+        // console.log(listItem);
+
+        const list = document.querySelector(".la-card");
+        list.appendChild(listItem);
+    });
+};
