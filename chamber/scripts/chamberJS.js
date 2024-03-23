a2ddeaca71fdf26adfb6fed63600093a50dad838
vern wolfley <vern.wolfley@gmail.com>
2024-03-22 23:07:34 -0700

    fix dark mode issues

--- chamber/scripts/chamberJS.js
+++ chamber/scripts/chamberJS.js
@@ -39,7 +39,7 @@ function screenMode() {
     element.classList.toggle("dark-mode");
 
     const sections = document.querySelectorAll(
-        "main, div.info, div.spotlights, div.event, article.card, div.weather-info, div.form-wrapper, section.member div.form-wrapper"
+        "main, div.info, div.spotlights, div.event, div.event-card, section.style5, article.card, div.weather-info, div.form-wrapper, section.member, section.member a, div.form-wrapper, form label.top input, form label.sbs, form fieldset, form legend, form label.top"
     );
     sections.forEach((section) => {
         section.classList.toggle("dark-mode");
